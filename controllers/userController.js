4125507b58db9561ff20e565095fdfdd0b50c0cc
ikram3031 <mdikram.3031@gmail.com>
2022-10-15 22:26:33 +0600

    get all users

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,29 +1,36 @@
+const User = require('../models/User');
+const { StatusCodes } = require('http-status-codes');
+const CustomError = require('../errors');
 
-
-const getAllUsers = async(req,res) => {
-    res.send(`get all users`);
+const getAllUsers = async (req, res) => {
+    const users = await User.find({ role: 'user' }).select('-password')
+    res.status(StatusCodes.OK).json({ users });
 }
 
-const getSingleUser = async(req,res) => {
-    res.send(`getSingleUser`);
+const getSingleUser = async (req, res) => {
+    const user = await User.find({ role: 'user' }).select('-passsword');
+    if (!user) {
+        throw new CustomError.NotFoundError(`No user with if: ${req.params.id}`);
+    }
+    res.status(StatusCodes.OK).json({ user });
 }
 
-const showCurrentUser = async(req,res) => {
+const showCurrentUser = async (req, res) => {
     res.send(`showCurrentUser`);
 }
 
-const updateUser = async(req,res) => {
+const updateUser = async (req, res) => {
     res.send(`updateSingleUser`);
 }
 
-const updateUserPassword = async(req,res) => {
+const updateUserPassword = async (req, res) => {
     res.send(`updateUserPassword`);
 }
 
 module.exports = {
-    getAllUsers, 
-    getSingleUser, 
-    showCurrentUser, 
-    updateUser, 
+    getAllUsers,
+    getSingleUser,
+    showCurrentUser,
+    updateUser,
     updateUserPassword
 }
